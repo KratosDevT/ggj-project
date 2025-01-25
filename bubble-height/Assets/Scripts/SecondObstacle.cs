95e3394b477bf413b9780c27f80804a8ea9b2570
porterSigma <davide.zamprogn02@gmail.com>
2025-01-25 15:47:48 +0100

    Second try thorny platform

--- bubble-height/Assets/Scripts/SecondObstacle.cs
+++ bubble-height/Assets/Scripts/SecondObstacle.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SecondObstacle : BaseObstacle
 {
+    private BoxCollider2D boxCollider;
+    private float movement;
+
+    [SerializeField]
+    Transform[] waypoints;
+    int waypoint_index = 0;
     void Start()
     {
         obstacleSpeed = speed * 0.5f; //it depends from the speed of the BaseObstacle
+        boxCollider = GetComponent<BoxCollider2D>();
+        movement = -obstacleSpeed * Time.deltaTime;
     }
     void Update()
     {
@@ -12,11 +21,19 @@ public class SecondObstacle : BaseObstacle
     }
     protected override void  Move()
     {
-        float movement = -obstacleSpeed * Time.deltaTime;
-        transform.Translate(movement, movement * 0.5f, 0);
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypoint_index].transform.position, movement);
+
+        if(transform.position == waypoints[waypoint_index].transform.position)
+        {
+            ++ waypoint_index;
+        }
+        if(waypoint_index == waypoints.Length)
+        {
+            return;
+        }
     }
     public override float getSize()
     {
-        return 0f;
+        return boxCollider.size.x * transform.localScale.x;
     }
 }
