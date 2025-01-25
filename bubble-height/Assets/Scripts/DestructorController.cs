85c0048ed2961af0917bd712638c8e94c712928a
Simone Trevisan <>
2025-01-25 19:27:18 +0100

    fix destructor and prefab

--- bubble-height/Assets/Scripts/DestructorController.cs
+++ bubble-height/Assets/Scripts/DestructorController.cs
@@ -4,9 +4,6 @@ public class DestructorController : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        //if (other.gameObject.tag == "Obstacle")
-        //{
-            Destroy(other.gameObject);
-        //}
+         Destroy(other.gameObject);
     }
 }
