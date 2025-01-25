3bd4f8393e8fc35d822623de2aeec143b7c05b60
Simone Trevisan <>
2025-01-25 12:25:22 +0100

    fix camera

--- bubble-height/Assets/Scripts/CharacterMovement.cs
+++ bubble-height/Assets/Scripts/CharacterMovement.cs
@@ -4,11 +4,11 @@ public class CharacterMovement : MonoBehaviour
 {
     //private float horizontal;
     [SerializeField]
-    private float speed = 1f;
+    private float speed = 1.0f;
     private Vector3 pos;
 
     [SerializeField]
-    private GameObject cameraObject;
+    private Camera cameraObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     /* void Start()
@@ -19,15 +19,10 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        float horizontalMovement = Input.GetAxis("Horizontal");
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
         //Character movement letf/right
         pos.x = transform.position.x + horizontalMovement * speed * Time.deltaTime;
-        pos.x = Mathf.Clamp(pos.x, -cameraObject.transform.position.x, cameraObject.transform.position.x);
+        pos.x = Mathf.Clamp(pos.x, -cameraObject.orthographicSize, cameraObject.orthographicSize);
         transform.position = pos;
-
-
     }
-
-
 }
