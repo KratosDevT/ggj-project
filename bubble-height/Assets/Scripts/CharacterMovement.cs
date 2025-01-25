c0917097f40b83ae61f2eeb958c64e6f5e74893c
Simone Trevisan <>
2025-01-25 15:58:59 +0100

    modified Bubble and background

--- bubble-height/Assets/Scripts/CharacterMovement.cs
+++ bubble-height/Assets/Scripts/CharacterMovement.cs
@@ -7,21 +7,25 @@ public class CharacterMovement : MonoBehaviour
     private float speed = 1.0f;
     private Vector3 pos;
 
-    //private GameObject cameraObject; 
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   /* void Start()
-    {
-        
-    }*/
+    [SerializeField]
+    private float leftBound = -2.0f;
+    [SerializeField]
+    private float rightBound = 2.0f;
 
-  private void Update()
+    //private GameObject cameraObject; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pos = new Vector3(0, transform.position.y, transform.position.z);
+    }
+
+    private void Update()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         //Character movement letf/right
         pos.x = transform.position.x + horizontalMovement * speed * Time.deltaTime;
-        pos.x = Mathf.Clamp(pos.x, -3.5f, 3.5f);
+        pos.x = Mathf.Clamp(pos.x, leftBound, rightBound);
         transform.position = pos;
     }
-   
 }
