d54b96955099cc760880fd200a288cd972aea82d
AhmetDomi <domi2904@hotmail.it>
2025-01-25 12:15:10 +0100

    Update CharacterMovement.cs

--- bubble-height/Assets/Scripts/CharacterMovement.cs
+++ bubble-height/Assets/Scripts/CharacterMovement.cs
@@ -5,9 +5,10 @@ public class CharacterMovement : MonoBehaviour
     //private float horizontal;
     [SerializeField]
     private float speed = 1f;
+    private Vector3 pos;
 
-    Vector3 pos;
-
+    private GameObject cameraObject; 
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    /* void Start()
     {
@@ -21,7 +22,7 @@ public class CharacterMovement : MonoBehaviour
         float horizontalMovement = Input.GetAxis("Horizontal");
         //Character movement letf/right
         pos.x = transform.position.x + horizontalMovement * speed * Time.deltaTime;
-        pos.x = Mathf.Clamp(pos.x, -3.5f, 3.5f);
+        pos.x = Mathf.Clamp(pos.x, -cameraObject.x, cameraObject.x);
         transform.position = pos;
         
 
