a2fe7dc66bd4f81b62e5dd5ed341c45c597669e1
BeCarlo <80776371+BeCarlo@users.noreply.github.com>
2025-01-26 01:44:07 +0100

    UI death

--- bubble-height/Assets/Scripts/Managers/GameManager.cs
+++ bubble-height/Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,12 +8,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject playerGameObject;
     [SerializeField] private GameObject backGroundGameObject;
     [SerializeField] private GameObject spawnerGameObject;
+    [SerializeField] private GameObject looseGameCanvas;
 
     [SerializeField] private float[] bgHighLevels;
     [SerializeField] private float backGroundVelocity;
     [SerializeField] private float currentHeight = 0;
     [SerializeField] private int stage = 0;
-    private static GameManager instance;
+    private static GameManager istance;
 
     private static float playerVelocityX;
     private static int playerLife = 10;
@@ -23,11 +25,15 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        instance = this;
+        istance = this;
 
         backgroundController = backGroundGameObject.GetComponent<BackgroundController>();
 
         AudioManager.Play(0);
+
+        Button btn = looseGameCanvas.GetComponentInChildren<Button>();
+        btn.onClick.AddListener(PlayAgain);
+
     }
 
     void Update()
@@ -47,20 +53,29 @@ public class GameManager : MonoBehaviour
     {
         if (--playerLife < 1) GameEnd();
 
-        if (playerLife % 2 == 0) AudioManager.Play(playerLife % 2);
+        if (playerLife % 2 == 0) AudioManager.Play((playerLife % 2) + 2);
 
         return playerLife;
     }
 
     private static void GameEnd()
     {
-        AudioManager.Play((int)AudioManager.Song.loose);
+        istance.looseGameCanvas.SetActive(true);
+
+        AudioManager.Play((int) AudioManager.Song.loose);
     }
 
     private static void GameEndWin()
     {
-        instance.StopGame();
+        istance.StopGame();
         AudioManager.Play((int)AudioManager.Song.win);
+    }
+
+    public void PlayAgain() {
+        istance.looseGameCanvas.SetActive(false);
+        //Clear Obstacle
+        //reset background
+        istance.UnPauseGame();
     }
 
     private void StopGame()
@@ -70,15 +85,20 @@ public class GameManager : MonoBehaviour
         playerGameObject.SetActive(false);
     }
 
+    private void UnPauseGame() {
+        spawnerGameObject.GetComponent<SpawnerScript>().enableSpawn();
+        backGroundGameObject.SetActive(true);
+        playerGameObject.SetActive(true);
+    }
+
     public static int getDifficulty()
     {
-        return instance.getCurrentStage();
+        return istance.getCurrentStage();
     }
 
     private int getCurrentStage()
     {
-        if (currentHeight < bgHighLevels[stage])
-        {
+        if (currentHeight < bgHighLevels[stage]) {
             ++stage;
         }
         return stage;
